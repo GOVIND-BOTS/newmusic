077b189552ff3a4dc46419de7f57a12cf7226df4
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2025-02-05 22:49:30 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -203,8 +203,8 @@ async def main():
         LOGGER.info(f"🚫 Assistant Error: {e}")
         sys.exit()
     try:
-        await app.join_chat("AdityaServer")
-        await app.join_chat("AdityaDiscus")
+        await app.join_chat("indian_chatting_club_offical")
+        await app.join_chat("taron_ka_shahar")
     except Exception:
         pass
     if LOG_GROUP_ID != 0:
@@ -221,7 +221,7 @@ async def main():
     LOGGER.info("✅ PyTgCalls Started.")
     await asyncio.sleep(1)
     LOGGER.info("✅ Sucessfully Hosted Your Bot !!")
-    LOGGER.info("✅ Now Do Visit: @AdityaServer !!")
+    LOGGER.info("✅ Now Do Visit: @indian_chatting_club_offical !!")
     await idle()
 
 
@@ -1361,7 +1361,7 @@ With Your ☛ Other Friends.**"""
             [
                 InlineKeyboardButton(
                     text="🌺 Open Repository Link 🦋",
-                    url="https://github.com/AdityaHalder/AdityaPlayer",
+                    url="https://github.com/GOVIND-BOTS/newmusic",
                 )
             ],
             [
